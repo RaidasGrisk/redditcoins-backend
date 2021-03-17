c648baf46f85fb82728d5d4ee0532e2db24042c9
RaidasGrisk <mrraidas@gmail.com>
2021-03-17 13:29:50 +0200

    move topics to separate file

---
+++ other_ops/topics.py
@@ -0,0 +1,32 @@
+
+topics = {
+    'GME': {'gme', 'gamestop', 'gamestonk'},
+    'AMC': {'amc'},
+    'PLTR': {'pltr', 'palantir'},
+    'SPY': {'spy'},
+    'DOGE': {'dogecoin'},
+    'crypto': {'coin', 'cypto', 'mining'},
+    'UVXY': {'uvxy'},
+    'SDOW': {'sdow'},
+    'SQQQ': {'sqqq'},
+    'SRTY': {'srty'},
+    'WSB': {'wsb'},
+
+    'GOOGL': {'googl'},
+    'AMZN': {'amzn'},
+    'AAPL': {'aapl'},
+    'NFLX': {'nflx'},
+    'MSFT': {'msft'},
+    'TSLA': {'tsla'},
+    'NVDA': {'nvda'},
+    'TECH': {'tech'},
+    'INTC': {'intc'},
+    'BABA': {'baba'},
+    'PYPL': {'pypl'},
+    'CSCO': {'csco'},
+    'MTCH': {'mtch'},
+    'ADBE': {'adbe'},
+    'DBX': {'dbx'},
+    'QQQ': {'qqq'},
+    'CRM': {'crm'}
+}
