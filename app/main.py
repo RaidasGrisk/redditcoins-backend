7ca83add0359b488d200d45fa71bf745d82967ba
RaidasGrisk <mrraidas@gmail.com>
2021-03-18 15:20:46 +0200

    update endpoint

--- app/main.py
+++ app/main.py
@@ -1,31 +1,81 @@
 '''
-https://github.com/mjhea0/awesome-fastapi#boilerplate
+https://pydantic-docs.helpmanual.io/usage/schema/#schema-customization
 '''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel
+from typing import List, Optional
 
-from db_to_timeseries import get_timeseries_df
+from app.db_to_timeseries import get_timeseries_df
+from other_ops.topics import topics
+
+# ok we need to parse ticker names and validate
+# that provided ticker is indeed valid and exists in db
+# TODO: this is too long and rendered docs are fucked
+valid_tickers = '|'.join(topics.keys())
+
+# valid granularity values
+# TODO: this is not a complete list but lets keep
+#  it short and simple for now. Not sure if this
+#  impacts the response speed. Most likely the biggest
+#  bottleneck is mongo query agg time. Fix that.
+granularities = ['Y', 'M', 'W', 'D', 'H', '6H', '2H']
+valid_granularity = '|'.join(granularities)
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def ping():
-    return {'Hi': '!'}
+    return {'message': 'Hey there!'}
 
 
-@app.get("/{ticker}")
-def read_item(
-        start: str,
-        end: str,
-        ticker: str,
-        ups: int,
-        submissions: bool,
-        comments: bool,
-        granularity: str
-) -> object:
+# the output data model example and validator
+class DataModelOut(BaseModel):
+    data: List[dict] = [
+        {'time': '2021-01-01', 'volume': 1},
+        {'time': '2021-01-02', 'volume': 2}
+    ]
 
-    df = get_timeseries_df(
+
+@app.get('/{ticker}', response_model=DataModelOut)
+async def timeseries(
+        ticker: str = Path(
+            ...,
+            title='ticker',
+            description='The name of the ticker e.g. NVDA, TSLA, GME',
+            # regex=f'({valid_tickers})',
+            include_in_schema=False
+        ),
+        start: str = Query(
+            ...,
+            description='The start of the time range to fetch data for, e.g. 2021-01-01'
+        ),
+        end: str = Query(
+            ...,
+            description='The end of the time range to fetch data for, e.g. 2021-02-01'
+        ),
+        ups: Optional[int] = Query(
+            10,
+            description='Include subs/comments with more than specified number of ups'
+        ),
+        submissions: bool = Query(
+            True,
+            description='Include submissions'
+        ),
+        comments: bool = Query(
+            False,
+            description='Include comments'
+        ),
+        granularity: str = Query(
+            'D',
+            description='Granularity of the data fetched',
+            regex=f'({valid_granularity})',
+        )
+) -> dict:
+
+    df = await get_timeseries_df(
         start=start,
         end=end,
         ticker=ticker,
@@ -35,7 +85,6 @@ def read_item(
         granularity=granularity
     )
 
-    # convert to str, else to_json will convert it to ts
-    df.index = df.index.astype(str)
-
-    return df.to_json()
+    return {
+        'data': df.reset_index().to_dict(orient='records')
+    }
