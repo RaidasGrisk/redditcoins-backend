9b4f1c84d408a7e1235b0ca5d4890e956dab52d2
raidas <mrraidas@gmail.com>
2023-06-01 20:11:46 +0300

    add cron extension and cron jobs

--- init_db.py
+++ init_db.py
@@ -2,7 +2,10 @@ import psycopg2
 from private import db_details
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-if __name__ == '__main__':
+
+def create_database_and_tables():
+
+    subreddits = ['cryptocurrency']
 
     con = psycopg2.connect(**db_details)
     # this or CREATE DATABASE cannot run inside a transaction block
@@ -30,7 +33,6 @@ if __name__ == '__main__':
                 body VARCHAR,
                 selftext VARCHAR
         """
-        subreddits = ['cryptocurrency']
         for subreddit in subreddits:
             cur.execute(
                 f'DROP TABLE IF EXISTS {subreddit};'
@@ -51,19 +53,119 @@ if __name__ == '__main__':
         # conversion on every query (speed tests show this
         # cuts time from 450 to 280 ms for 10 mln rows)
 
-        # Let's set the id to unique identifier so that
-        # when updating topic values we do not add new
-        # records everytime we do the op over table with
-        # already present records.
-        cols = """
-                _id VARCHAR PRIMARY KEY,
-                created_utc INT,
-                is_comment BOOLEAN,
-                topic VARCHAR
-        """
-        for subreddit in [sub + '_' for sub in subreddits]:
+        con = psycopg2.connect(**db_details, dbname='reddit')
+        con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+        with con.cursor() as cur:
+
+            # Let's set the id to unique identifier so that
+            # when updating topic values we do not add new
+            # records everytime we do the op over table with
+            # already present records.
+            cols = """
+                    _id VARCHAR PRIMARY KEY,
+                    created_utc INT,
+                    is_comment BOOLEAN,
+                    topic VARCHAR
+            """
+            for subreddit in [sub + '_' for sub in subreddits]:
+                cur.execute(
+                    f'DROP TABLE IF EXISTS {subreddit};'
+                    f'CREATE TABLE IF NOT EXISTS {subreddit} ({cols})'
+                )
+
+
+def create_cron_jobs():
+
+    cron_jobs = [
+        {
+            'name': 'hourly_data',
+            'aggregation_helper': 'hour',
+            'lookback_time_in_hours': 24,
+            'cron_scedule': '5 * * * *'  # every hour at 5 minutes
+        },
+        {
+            'name': 'daily_data',
+            'aggregation_helper': 'day',
+            'lookback_time_in_hours': 24 * 24,
+            'cron_scedule': '5 1 * * *'  # every day at 1 hour 5 min
+        },
+    ]
+
+    # Inside the reddit database:
+    # 1. Create functions to be run by cron inside the reddit database
+    con = psycopg2.connect(**db_details, dbname='reddit')
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    with con.cursor() as cur:
+
+        for job in cron_jobs:
+
+            query = f"""
+                DROP TABLE IF EXISTS {job['name']};
+                CREATE TABLE {job['name']} AS
+                WITH last_period AS (
+                    SELECT (
+                        DATE_TRUNC('{job['aggregation_helper']}', CURRENT_TIMESTAMP AT TIME ZONE 'UTC') 
+                        - INTERVAL '0 {job['aggregation_helper']}'
+                    )::timestamp AS last_timestamp
+                ),
+                data_ AS (
+                    SELECT * FROM cryptocurrency_
+                    WHERE
+                        created_utc <= EXTRACT(EPOCH FROM (SELECT last_timestamp FROM last_period))
+                        AND created_utc > EXTRACT(EPOCH FROM (SELECT last_timestamp FROM last_period)) 
+                        - ({job['lookback_time_in_hours']} * 60 * 60)
+                )
+                SELECT 
+                    topic,
+                    DATE_TRUNC('{job['aggregation_helper']}', TIMESTAMP 'epoch' + created_utc * INTERVAL '1 second') AS gran,
+                    COUNT(*) AS count
+                FROM data_
+                GROUP BY gran, topic
+                ORDER BY topic, gran DESC;
+            """
+
             cur.execute(
-                f'DROP TABLE IF EXISTS {subreddit};'
-                f'CREATE TABLE IF NOT EXISTS {subreddit} ({cols})'
+                f"""
+                CREATE OR REPLACE FUNCTION {job['name']}()
+                RETURNS VOID AS
+                $$
+                BEGIN
+                    {query}
+                END;
+                $$
+                LANGUAGE plpgsql;
+                """
             )
 
+    # Inside the postgres database:
+    # 2. Activate cron extensions, remove all cron jobs (if any)
+    # 3. Create cron jobs
+    con = psycopg2.connect(**db_details, dbname='postgres')
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    with con.cursor() as cur:
+
+        cur.execute(
+            """
+            CREATE EXTENSION IF NOT EXISTS pg_cron;
+            TRUNCATE TABLE cron.job;
+            TRUNCATE TABLE cron.job_run_details;
+            """
+        )
+
+        for job in cron_jobs:
+            cur.execute(
+                f"""
+                SELECT cron.schedule_in_database(
+                    '{job['name']}',
+                    '5 * * * *',
+                    'SELECT {job['name']}()',
+                    'reddit'
+                );
+                """
+            )
+
+
+if __name__ == '__main__':
+    create_database_and_tables()
+    create_cron_jobs()
